f57fc25869335fa420a92ff2c4fcc74abf925b43
Pallavi Rangadiya <90688147+pallavirivulet@users.noreply.github.com>
2023-06-20 15:23:07 +0530

    add filter related code in ticket index api.

---
+++ app/Filters/TicketFilter.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Filters;
+use Pricecurrent\LaravelEloquentFilters\AbstractEloquentFilter;
+use Illuminate\Database\Eloquent\Builder;
+
+class TicketFilter extends AbstractEloquentFilter
+{
+    protected $request;
+
+    public function __construct($request)
+    {
+        $this->request = $request;
+    }
+
+    public function apply(Builder $builder): Builder
+    {
+        $ticketQuery = $builder->with(['customer.phones'=> function($qry){ $qry->select('id','customer_id','phone'); },
+                'customer_location',
+                'assigned_engineer'=> function($qry){ $qry->select('id','first_name','last_name','profile_photo'); },
+                'appointment_type' => function($qry){ $qry->select('id','appointment_name'); },
+                'ticket_priority' => function($qry){ $qry->select('id','priority_name'); },
+                'ticket_status' => function($qry){ $qry->select('id','status_name'); },
+                'payment_status' => function($qry){ $qry->select('id','payment_type'); }]);
+
+        if(isset($this->request->customer_id) && (count($this->request->customer_id) > 0)){
+            $ticketQuery = $ticketQuery->whereIn('customer_id',$this->request->customer_id);
+        }
+        if(isset($this->request->problem_type_id) && (count($this->request->problem_type_id) > 0)){
+            $ticketQuery = $ticketQuery->whereIn('problem_type_id',$this->request->problem_type_id);
+        }
+        if(isset($this->request->ticket_status_id) && (count($this->request->ticket_status_id) > 0)){
+            $ticketQuery = $ticketQuery->whereIn('ticket_status_id',$this->request->ticket_status_id);
+        }
+        if(isset($this->request->appointment_type_id) && (count($this->request->appointment_type_id) > 0)){
+            $ticketQuery = $ticketQuery->whereIn('appointment_type_id',$this->request->appointment_type_id);
+        }
+        if(isset($this->request->payment_type_id) && (count($this->request->payment_type_id) > 0)){
+            $ticketQuery = $ticketQuery->whereIn('payment_type_id',$this->request->payment_type_id);
+        }
+        if(isset($this->request->priority_id) && (count($this->request->priority_id) > 0)){
+            $ticketQuery = $ticketQuery->whereIn('priority_id',$this->request->priority_id);
+        }
+        return $ticketQuery;
+    }
+}
--- app/Models/Tickets.php
+++ app/Models/Tickets.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 use App\Enums\TicketTypesEnum;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Pricecurrent\LaravelEloquentFilters\Filterable;
 
 class Tickets extends Model
 {
-    use HasFactory,SoftDeletes;
+    use HasFactory,SoftDeletes,Filterable;
 //    protected $casts = ['ticket_type' => TicketTypesEnum::class];
     protected $fillable = ['id','url_slug','ticket_type','customer_id','customer_locations_id','problem_type_id',
         'ticket_status_id','priority_id','assigned_user_id','appointment_type_id','payment_type_id','problem_title','due_date',
