1de9ffe5c2a9fe4ec24637c44dd8099b824cdce6
Pallavi Rangadiya <90688147+pallavirivulet@users.noreply.github.com>
2023-07-06 18:46:51 +0530

    refactor dashboard ticket status api.

--- app/Repositories/Dashboard/DashboardRepository.php
+++ app/Repositories/Dashboard/DashboardRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Dashboard;
 
 use App\Models\Tickets;
 use App\Models\TicketStatus;
+use DB;
 
 class DashboardRepository implements DashboardRepositoryInterface{
     public function getDetails(){
@@ -17,19 +18,25 @@ class DashboardRepository implements DashboardRepositoryInterface{
     }
 
     public function getTicketStatus(){
-        //dd(Tickets::query()->select('ticket_status_id')->groupBy('ticket_status_id')->selectRaw('count(*) as ticket_status_count')->get()->pluck('ticket_status_count','ticket_status_id')->toArray());
-//        dd(TicketStatus::select('id','status_name')->withCount('tickets')->get()->pluck('tickets_count','status_name')->toArray());
         $data = [
-          'total_ticket' => Tickets::count(),
-          'open_ticket' => Tickets::where('ticket_status_id',1)->count(),
-          'open_ticket_percentage' => Tickets::where('ticket_status_id',1)->count() / Tickets::count() * 100,
-          'inprogress_ticket' => Tickets::where('ticket_status_id',2)->count(),
-          'inprogress_ticket_percentage' => Tickets::where('ticket_status_id',2)->count() / Tickets::count() * 100,
-          'workdone_ticket' => Tickets::where('ticket_status_id',3)->count(),
-          'workdone_ticket_percentage' => Tickets::where('ticket_status_id',3)->count() / Tickets::count() * 100,
-          'closed_ticket' => Tickets::where('ticket_status_id',4)->count(),
-          'closed_ticket_percentage' => Tickets::where('ticket_status_id',4)->count() / Tickets::count() * 100,
+            'total_ticket_count' => Tickets::count(),
+            'ticket_status_by_percentage' => TicketStatus::select('status_name as status')->withCount('tickets as count')
+                ->get()
+                ->map(function ($item) {
+                    $item->percentage = ($item->count / Tickets::count() * 100);
+                    return $item;
+                })
+                ->toArray()
         ];
         return $data;
+//        $ticketCount = Tickets::count();
+//        $ticketStatusByPercentage = TicketStatus::select('status_name as status')->withCount('tickets as count')->get();
+//        $ticketStatusByPercentage->map(function ($item) use($ticketCount) {
+//            $item->percentage = ($item->count / $ticketCount * 100);
+//            return $item;
+//        });
+//        $data['ticket_status_by_percentage'] = $ticketStatusByPercentage->toArray();
+//        $data['total_ticket_count'] = $ticketCount;
+//        return $data;
     }
 }
