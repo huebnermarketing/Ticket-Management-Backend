c68ca180ac0f836d78d0f71d5870749febc39151
Pallavi Rangadiya <90688147+pallavirivulet@users.noreply.github.com>
2023-07-06 13:51:28 +0530

    refactore contract code.

--- app/Models/TicketProblemType.php
+++ app/Models/TicketProblemType.php
@@ -2,11 +2,19 @@
 
 namespace App\Models;
 
+use App\Traits\CommonTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TicketProblemType extends Model
 {
-    use HasFactory;
-    protected $fillable = ['id','ticket_id','problem_type_id','created_at','updated_at'];
+    use HasFactory,CommonTrait;
+    protected $fillable = ['id','unique_id','ticket_id','problem_type_id','created_at','updated_at'];
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->unique_id = static::generateId();
+        });
+    }
 }
--- app/Repositories/Contract/ContractRepository.php
+++ app/Repositories/Contract/ContractRepository.php
@@ -23,19 +23,17 @@ class ContractRepository implements ContractRepositoryInterface
 //        $customers = Customers::join('customer_phones', 'customer_phones.customer_id', 'customers.id')
 //            ->where('customer_phones.is_primary',1)->select('customers.*','customer_phones.customer_id','customer_phones.phone')->get();
 //        $data['customers'] = $customers;
+        $contractType = ContractType::select('id', 'contract_name')->get();
+        $productService = ProductServices::select('id', 'service_name')->get();
+        $contractDuration = ContractDuration::select('id', 'slug', 'display_name')->get();
+        $contractPaymentTerms = ContractPaymentTerm::select('id', 'slug', 'display_name')->get();
 
-        $contractType = ContractType::select('id','contract_name')->get();
-        $data['contract_services'] = $contractType;
-
-        $productService = ProductServices::select('id','service_name')->get();
-        $data['product_services'] = $productService;
-
-        $contractDuration = ContractDuration::select('id','slug','display_name')->get();
-        $data['contract_duration'] = $contractDuration;
-
-        $contractPaymentTerms = ContractPaymentTerm::select('id','slug','display_name')->get();
-        $data['contract_payment_terms'] = $contractPaymentTerms;
-        return $data;
+        return [
+            'contract_services' => $contractType,
+            'product_services' => $productService,
+            'contract_duration' => $contractDuration,
+            'contract_payment_terms' => $contractPaymentTerms,
+        ];
     }
 
     public function getContracts($request){
@@ -44,7 +42,6 @@ class ContractRepository implements ContractRepositoryInterface
         $customers = Customers::withCount(['contract' => function($query) use($type){
             $query->whereIn('contract_status_id',$type);
         }])->having('contract_count', '>', 0)->orderBy('first_name','asc')->paginate(config('constant.PAGINATION_RECORD'));
-        $data['allClient'] = $customers;
         $clientDashboard = [
             'active_contract' => $contracts->count(),
             'paid_amount' => $contracts->sum('amount'),
@@ -53,7 +50,10 @@ class ContractRepository implements ContractRepositoryInterface
                     $query->where('contract_status_id',getStatusId(10001)->id);
                     }])->where(['ticket_type'=>'contract'])->whereNot('ticket_status_id','4')->count()
         ];
-        $data['client_dashboard'] = $clientDashboard;
+        $data = [
+            'all_client'=>$customers,
+            'client_dashboard'=>$clientDashboard
+        ];
         return $data;
     }
 
@@ -62,7 +62,6 @@ class ContractRepository implements ContractRepositoryInterface
         if(!empty($customerData)) {
             $filterQuery = EloquentFilters::make([new ContractStatusFilter($request)]);
             $contractList = Contract::filter($filterQuery)->orderBy('id', 'asc')->get();
-            $data['contract_list'] = $contractList;
             $contractDashboard = [
                 'client_name'=> $customerData['first_name'] . ' ' . $customerData['last_name'],
                 'client_total_active_contract' => $contractList->count(),
@@ -70,7 +69,10 @@ class ContractRepository implements ContractRepositoryInterface
                 'remaining_amount' => $contractList->sum('remaining_amount'),
                 'open_contract_ticket' => Tickets::where(['customer_id' => $request['customer_id'], 'ticket_type' => 'contract'])->whereNot('ticket_status_id', 4)->count()
             ];
-            $data['contract_dashboard'] = $contractDashboard;
+            $data = [
+                'contract_list'=> $contractList,
+                'contract_dashboard'=>$contractDashboard
+            ];
             return $data;
         }else{
             return false;
@@ -78,28 +80,31 @@ class ContractRepository implements ContractRepositoryInterface
     }
 
     public function viewContract($data){
-
         $contractDetails = Contract::with([
-            'customers' => function ($qry) {
-                $qry->with('phones', function ($query){
-                    $query->where("is_primary",1);
-                    $query->select('id','customer_id','phone','is_primary');
-                });
-                $qry->select('id','first_name','last_name','email');
-            },'customerLocation:id,customer_id,company_name,address_line1,area,zipcode,city,state,country',
-            'contractServicesTypes' => function ($request) {
-                $request->with('contractTypes:id,contract_name')
-                    ->select('id','contract_id','contract_type_id');
-            },'productService' => function($query){
-                $query->with('productService:id,unique_id,service_name')
-                ->select('id','contract_id','product_service_id','product_qty','product_amount');
-            },'duration:id,slug,display_name','paymentTerm:id,slug,display_name'
-            ])->select('id','unique_id','customer_id','customer_location_id','contract_title','contract_details','amount','duration_id',
-            'payment_term_id','contract_status_id','start_date','end_date','is_auto_renew','open_ticket_contract','is_suspended')
-            ->where('id',$data['contract_id'])->first();
+            'customers' => function ($q) {
+                $q->select('id', 'first_name', 'last_name', 'email')
+                    ->with('phones', function ($q) {
+                        $q->where('is_primary', 1)
+                            ->select('id', 'customer_id', 'phone', 'is_primary');
+                    });
+            },
+            'customerLocation:id,customer_id,company_name,address_line1,area,zipcode,city,state,country',
+            'contractServicesTypes' => function ($q) {
+                $q->select('id', 'contract_id', 'contract_type_id')
+                    ->with('contractTypes:id,contract_name');
+            },
+            'productService' => function ($q) {
+                $q->select('id', 'contract_id', 'product_service_id', 'product_qty', 'product_amount')
+                    ->with('productService:id,unique_id,service_name');
+            },
+            'duration:id,slug,display_name',
+            'paymentTerm:id,slug,display_name'
+        ])->select('id', 'unique_id', 'customer_id', 'customer_location_id', 'contract_title', 'contract_details', 'amount', 'duration_id', 'payment_term_id', 'contract_status_id', 'start_date', 'end_date', 'is_auto_renew', 'open_ticket_contract', 'is_suspended')
+            ->where('id', $data['contract_id'])
+            ->first();
 
         /*suspend button logic
-        first check contract active or archive
+        first check contract active
         then check contract ticket all are closed*/
         $contractstatus = Contract::where(['id' => $data['contract_id'],'contract_status_id'=>getStatusId(10001)->id])->first();
         if($contractstatus != null){
@@ -111,8 +116,10 @@ class ContractRepository implements ContractRepositoryInterface
         }else{
             $isSuspended = false;
         }
-        $contractData['suspend_flag'] = $isSuspended;
-        $contractData['contract_details'] = $contractDetails;
+        $contractData = [
+            'suspend_flag'=>$isSuspended,
+            'contract_details' => $contractDetails
+        ];
         return $contractData;
     }
 
